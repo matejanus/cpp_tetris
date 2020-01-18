be4166c0b59517ed2e4b0f261e19f585301ad4d7
Mateusz <mate.janus@gmail.com>
2020-01-18 21:45:41 +0100

    add tetris blocks definition

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 #include "includes/defines.hpp"
+#include <array>
 
+std::array<std::string, 7> tetromino; 
 int main()
 {
-    std::cout<<"hi";
+    tetromino[0].append("..X.");
+    tetromino[0].append("..X.");
+    tetromino[0].append("..X.");
+    tetromino[0].append("..X.");
+
+    tetromino[1].append("..X.");
+    tetromino[1].append(".XX.");
+    tetromino[1].append(".X..");
+    tetromino[1].append("....");
+
+    tetromino[2].append(".X..");
+    tetromino[2].append(".XX.");
+    tetromino[2].append("..X.");
+    tetromino[2].append("....");
+
+    tetromino[3].append("....");
+    tetromino[3].append(".XX.");
+    tetromino[3].append(".XX.");
+    tetromino[3].append("....");
+
+    tetromino[4].append("..X.");
+    tetromino[4].append(".XX.");
+    tetromino[4].append("..X.");
+    tetromino[4].append("....");
+
+    tetromino[5].append("....");
+    tetromino[5].append(".XX.");
+    tetromino[5].append("..X.");
+    tetromino[5].append("..X.");
+
+    tetromino[6].append("....");
+    tetromino[6].append("..XX");
+    tetromino[6].append("..X.");
+    tetromino[6].append("..X.");
+
     return 0;
 }
